2acb5319247922cd21044ed40bfda3a6f41bc8c2
Ishita Prasad <ishitaprasad421@gmail.com>
2026-01-28 01:46:30 +0530

    Update leetcode414.py

--- leetcode414.py
+++ leetcode414.py
@@ -1,11 +1,12 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         nums.sort()
-        unique = []
+        unique=[]
         for i in nums:
             if i not in unique:
                 unique.append(i)
-        if len(unique) >= 3:
+        if len(unique)>=3:
             return unique[-3]
         else:
+
             return unique[-1]   
