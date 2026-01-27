740a928793e433c96788c69ba88d3c473d60b680
Ishita Prasad <ishitaprasad421@gmail.com>
2026-01-28 01:45:52 +0530

    Add files via upload

---
+++ leetcode414.py
@@ -0,0 +1,11 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums.sort()
+        unique = []
+        for i in nums:
+            if i not in unique:
+                unique.append(i)
+        if len(unique) >= 3:
+            return unique[-3]
+        else:
+            return unique[-1]   
